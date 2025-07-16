3a8b0ce33669e4d270ee347bdc65285cb23e1aa1
RokiaDiakite <diakiter928@gmail.com>
2025-07-16 15:34:07 +0000

    envoie du projet final

--- ajouter.php
+++ ajouter.php
@@ -11,11 +11,17 @@
         <div class="contenue">
                 <a class="lien" href="index.php">Cliquer pour voir la liste de tous livres</a>
                 <h1>AJOUT D'UN LIVRE DANS LA BASE</h1>
+                <?php if (isset($_GET['erreur'])): ?>
+                    <div class="echou">
+                        <?= htmlspecialchars($_GET['erreur']) ?>
+                    </div>
+                <?php endif; ?>
+
             <form action="enregistrer.php" method="post">
                 <label for="">TITRE</label>
-                <input type="text" name="titre" placeholder="titre du livre">
+                <input type="text" name="titre" placeholder="Titre du livre" required>
                 <label for="">DATE SORTIE</label>
-                <input type="date" name="dat_sortie" placeholder="Entrez la date de sortie">
+                <input type="date" name="dat_sortie" required>
                 <input type="submit" name="enregistrer" value="enregistrer">
             </form>
         </div>
